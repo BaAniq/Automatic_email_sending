a181585adecca8a37bfcdfbf1ff250c98ab1b43c
BaAniq <basia.anisimowicz@gmail.com>
2023-12-16 11:29:45 +0100

    Sending an email on Saturday with a quote of the day

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from random import choice
+from datetime import datetime
+from smtplib import SMTP
+
+my_email = "dimitoret@gmail.com"
+password = "aptz giun elit jebb"
+
 
 try:
     with open('Left_quotes.txt', 'r+') as left_quotes:
@@ -11,8 +17,17 @@ except FileNotFoundError:
             left_quotes.write(quote)
 
 quote_of_day = choice(quotes_list)
-print(quote_of_day)
 quotes_list.remove(quote_of_day)
 with open("Left_quotes.txt", 'w') as left_quotes:
     for quote in quotes_list:
         left_quotes.write(quote)
+
+today = datetime.now()
+weekday = today.weekday()
+if weekday == 5:
+    with SMTP('smtp.gmail.com', 587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, to_addrs='dimitoret@op.pl', msg=f'Subject:Quote of the day \n\n '
+                                                                                f'This is the quote of the day: \n'
+                                                                                f' {quote_of_day} ')
